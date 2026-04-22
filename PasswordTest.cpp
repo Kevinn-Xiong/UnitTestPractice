5c3e548849dabb8e05170c656edca95dcd9431c1
Kevin Xiong <kxiong10@csuchico.edu>
2026-04-22 12:02:17 -0700

    after coverage

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -28,9 +28,28 @@ TEST(PasswordTest, mixed_case_password)
 	ASSERT_EQ(2, actual);
 }
 
-TEST(PasswordTest, has_mixed_case)
+/*TEST(PasswordTest, has_mixed_case)
 {
 	Password my_password;
 	int actual = my_password.has_mixed_case("zZzZz");
 	ASSERT_TRUE(actual);
+}*/
+
+TEST(PasswordTest, cover_lowercase)
+{
+	Password my_password;
+	int actual = my_password.has_mixed_case("AAAbb");
+	ASSERT_EQ(2,actual);
+}
+
+TEST(PasswordTest, NoLetters) {
+    Password my_password;
+    EXPECT_FALSE(my_password.has_mixed_case("12345"));
+    EXPECT_FALSE(my_password.has_mixed_case("!@#$%"));
+}
+
+TEST(PasswordTest, OnlyOneLetterCasePresentWithSymbols) {
+    Password p;
+    EXPECT_FALSE(p.has_mixed_case("abc123!"));
+    EXPECT_FALSE(p.has_mixed_case("ABC123!"));
 }
